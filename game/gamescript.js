97fdb19177606585fcb995d148e996ec6801347a
Lucyna Maliszewska <lucyna.malisz@gmail.com>
2019-06-17 15:47:49 +0200

    "modal instruction added"

--- game/gamescript.js
+++ game/gamescript.js
@@ -1,24 +1,15 @@
 // instruction (Lucyna)
-
-
-
-
+var modal = document.getElementById("myModal");
+var opensModal = document.getElementById("instrukcja");
+opensModal.onclick = function() {
+  modal.style.display = "block";
+};
 // instruction-end
-
 
 // game (Damian)
 
-
-
-
-
 // game-end(Damian)
 
-
-
 // score (Asia)
-
-
-
 
 // score-end
