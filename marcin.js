f727371ce4b2ba5962e9d5ef60cb70301346b3ef
Marcin Pietkiewicz <marcin.p.dev@gmail.com>
2019-06-18 16:44:48 +0200

    Added back-to-top button to index.html

--- marcin.js
+++ marcin.js
@@ -8,3 +8,27 @@ burgerButton.addEventListener("click", toggleMenu);
 function toggleMenu() {
   menuList.classList.toggle("menu-fold");
 }
+
+let scrollButton = document.querySelector("#scrollButton");
+scrollButton.addEventListener("click", topFunction);
+
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 500 ||
+    document.documentElement.scrollTop > 500
+  ) {
+    scrollButton.style.display = "block";
+  } else {
+    scrollButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
