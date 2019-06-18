0eb602023aa28297282de3db2a0182b982cded46
Lucyna Maliszewska <lucyna.malisz@gmail.com>
2019-06-18 15:42:07 +0200

    "nothing important"

--- game/gamescript.js
+++ game/gamescript.js
@@ -4,10 +4,14 @@ var opensModal = document.getElementById("instrukcja");
 opensModal.onclick = function() {
   modal.style.display = "block";
 };
-var closeButton = document.getElementsByClassName("exit")[0]
+var closeButton = document.getElementsByClassName("exit")[0];
 closeButton.onclick = function() {
-  modal.style.display = "none"
-}
+  modal.style.display = "none";
+};
+//var wynikiButton = document.getElementsByClassName("wyniki")[0];
+//wynikiButton.onclick = function() {
+
+//}
 // instruction-end
 
 // game (Damian)
