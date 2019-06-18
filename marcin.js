8446997e4fb16b2ffdca9e81de9dd48bca6c0a78
Marcin Pietkiewicz <marcin.p.dev@gmail.com>
2019-06-18 15:02:58 +0200

    Added js toggle to mobile menu

--- marcin.js
+++ marcin.js
@@ -1,1 +1,19 @@
-console.log("hello Marcin jsfile  po Lucynie");
+// function mobileLogoDisplay() {
+
+let burgerButton = document.querySelector("#burger");
+let menuList = document.querySelector("ul");
+
+burgerButton.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  console.log(menuList);
+  if (menuList.style.display === "flex") {
+    menuList.style.display = "none";
+    menuList.style.transition = "0.5s";
+  } else {
+    menuList.style.display = "flex";
+    menuList.style.transition = "0.5s";
+  }
+
+  console.log("im togglging!");
+}
