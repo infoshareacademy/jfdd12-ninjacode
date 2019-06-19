6d6c2b4279a586f25ac73feca139ca82ce899592
LucynaMaliszewska <48649305+LucynaMaliszewska@users.noreply.github.com>
2019-06-19 16:42:19 +0200

    modal scores added and path from instruction to scores (#1)
    * modal scores added and path from instruction to scores
    
    * pull request fixed

--- game/gamescript.js
+++ game/gamescript.js
@@ -1,13 +1,25 @@
 // instruction (Lucyna)
-var modal = document.getElementById("myModal");
-var opensModal = document.getElementById("instrukcja");
-opensModal.onclick = function () {
-  modal.style.display = "block";
-};
-var closeButton = document.getElementsByClassName("exit")[0];
-closeButton.onclick = function () {
-  modal.style.display = "none";
-};
+const instructionModal = document.getElementById("instruction-modal");
+const scoresModal = document.getElementById("scores-modal");
+const instructionBtn = document.getElementById("instruction-btn");
+const scoresBtn = document.getElementById("scores-btn");
+const closeButton = document.getElementById("exit-btn");
+
+function displayScores() {
+  instructionModal.style.display = "none";
+  scoresModal.classList.toggle("scores-modal-shown");
+}
+
+scoresBtn.addEventListener("click", displayScores);
+
+instructionBtn.addEventListener("click", function() {
+  instructionModal.style.display = "block";
+});
+
+closeButton.addEventListener("click", function() {
+  instructionModal.style.display = "none";
+});
+
 //var wynikiButton = document.getElementsByClassName("wyniki")[0];
 //wynikiButton.onclick = function() {
 
@@ -15,60 +27,56 @@ closeButton.onclick = function () {
 // instruction-end
 
 // game (Damian)
-const GAME_WIDTH = 288
-const GAME_HEIGHT = 512
-const FLOOR_HEIGHT = 112
-const FLOOR_WIDTH = 336
-const PLAYER_HEIGHT = 100
-const PLAYER_WIDTH = 49
-const IMAGES_PATH = "/game/img/"
-const GRAVITY = 0.2
-const COIN_WIDTH = 440
-const COIN_HEIGHT = 40
-
+const GAME_WIDTH = 288;
+const GAME_HEIGHT = 512;
+const FLOOR_HEIGHT = 112;
+const FLOOR_WIDTH = 336;
+const PLAYER_HEIGHT = 100;
+const PLAYER_WIDTH = 49;
+const IMAGES_PATH = "/game/img/";
+const GRAVITY = 0.2;
+const COIN_WIDTH = 440;
+const COIN_HEIGHT = 40;
 
 // instruction-end
 
 // Canvas on global
-const canvas = document.createElement("canvas")
-canvas.setAttribute("height", `${GAME_HEIGHT}px`)
-canvas.setAttribute("width", `${GAME_WIDTH}px`)
-this.context = canvas.getContext("2d")
-const gameCanvas = document.querySelector("#game")
-gameCanvas.append(canvas)
+const canvas = document.createElement("canvas");
+canvas.setAttribute("height", `${GAME_HEIGHT}px`);
+canvas.setAttribute("width", `${GAME_WIDTH}px`);
+this.context = canvas.getContext("2d");
+const gameCanvas = document.querySelector("#game");
+gameCanvas.append(canvas);
 
-const body = document.querySelector("body")
-let direction
+const body = document.querySelector("body");
+let direction;
 
-body.addEventListener('keydown', event => {
-  console.log(event)
-  if (event.key === 'ArrowRight') {
+body.addEventListener("keydown", event => {
+  console.log(event);
+  if (event.key === "ArrowRight") {
     moveRight();
-    direction = 1
-  } else if (event.key === 'ArrowLeft') {
-    moveLeft()
+    direction = 1;
+  } else if (event.key === "ArrowLeft") {
+    moveLeft();
+  } else direction = 0;
+});
 
-  } else
-    direction = 0
-})
-
-function draw(imageUrl, x, y, w, h, onload = () => { }) {
-
-  const image = new Image()
-  image.src = `${IMAGES_PATH}${imageUrl}`
-  image.onload = function () {
-    context.drawImage(image, x, y, w, h)
-    onload()
-  }
+function draw(imageUrl, x, y, w, h, onload = () => {}) {
+  const image = new Image();
+  image.src = `${IMAGES_PATH}${imageUrl}`;
+  image.onload = function() {
+    context.drawImage(image, x, y, w, h);
+    onload();
+  };
 }
 // game (Damian)
 const player = {
   x: (GAME_WIDTH - PLAYER_WIDTH) / 2,
   y: GAME_HEIGHT - FLOOR_HEIGHT - PLAYER_HEIGHT
-}
+};
 
 function drawPlayer() {
-  draw('cashBakeMan.png', player.x, player.y, PLAYER_WIDTH, PLAYER_HEIGHT);
+  draw("cashBakeMan.png", player.x, player.y, PLAYER_WIDTH, PLAYER_HEIGHT);
 }
 
 function moveRight() {
@@ -76,83 +84,72 @@ function moveRight() {
     player.x = player.x + 10;
     console.log(player);
   } else {
-    player.x = GAME_WIDTH - PLAYER_WIDTH
+    player.x = GAME_WIDTH - PLAYER_WIDTH;
   }
-
 }
 function moveLeft() {
   if (player.x > 0) {
-    player.x -= 10
-    console.log(player)
+    player.x -= 10;
+    console.log(player);
   } else {
-    player.x = 0
+    player.x = 0;
   }
 }
-
 
 function GameArea(
   width,
   height,
   difficultyLevel = "easy" /* values: 'easy', 'hard' */
 ) {
-  this.width = width
-  this.height = height
-  this.difficultyLevel = difficultyLevel //TODO: add difficulty choice in begin game
-  this.generateCanvas()
-
+  this.width = width;
+  this.height = height;
+  this.difficultyLevel = difficultyLevel; //TODO: add difficulty choice in begin game
+  this.generateCanvas();
 }
 
 GameArea.prototype = {
-  drawMyImage: function (imageUrl, x, y, w, h, onload = () => { }) {
-    this.x = x
-    this.y = y
-    this.w = w
+  drawMyImage: function(imageUrl, x, y, w, h, onload = () => {}) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
 
-    const image = new Image()
-    image.src = `${IMAGES_PATH}${imageUrl}`
-    image.onload = function () {
-      context.drawImage(image, x, y, w, h)
-      onload()
-    }
+    const image = new Image();
+    image.src = `${IMAGES_PATH}${imageUrl}`;
+    image.onload = function() {
+      context.drawImage(image, x, y, w, h);
+      onload();
+    };
   },
-  drawFloor: function (context) {
+  drawFloor: function(context) {
     const floorImage = this.drawMyImage(
       "floor.png",
       0,
       GAME_HEIGHT - FLOOR_HEIGHT,
       FLOOR_WIDTH,
       FLOOR_HEIGHT
-    )
+    );
   },
-  drawBackground: function (context) {
+  drawBackground: function(context) {
     const backgroundImage = this.drawMyImage(
       "background-day.png",
       0,
       0,
       GAME_WIDTH,
       GAME_HEIGHT
-    )
+    );
   },
-  generateCanvas: function () {
-
-    this.drawBackground(this.context)
-    this.drawFloor(this.context)
-
-
+  generateCanvas: function() {
+    this.drawBackground(this.context);
+    this.drawFloor(this.context);
   },
-  gameLoop: function (time) {
-
+  gameLoop: function(time) {
     drawPlayer();
-    requestAnimationFrame(this.gameLoop.bind(this))
+    requestAnimationFrame(this.gameLoop.bind(this));
   }
-}
+};
 
-const game = new GameArea(600, 500, "easy")
-game.gameLoop()
-
-
-
-
+const game = new GameArea(600, 500, "easy");
+game.gameLoop();
 
 //TODO: add  loop function
 
