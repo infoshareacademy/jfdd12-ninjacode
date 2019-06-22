90e5813d49cd2b90b8ce368ae3dba3525af5d5f6
Joanna Skrzypińska <jm.skrzypinska@gmail.com>
2019-06-22 20:21:58 +0200

    game loop reworked, added coin generation and colision detection, animation fixes

--- game/gamescript.js
+++ game/gamescript.js
@@ -4,10 +4,12 @@ const scoresModal = document.getElementById("scores-modal");
 const instructionBtn = document.getElementById("instruction-btn");
 const scoresBoardBtn = document.getElementById("scores-board-btn");
 const scoresBtn = document.getElementById("scores-btn");
-const scoreTxt = document.getElementById("score-number")
+const scoreTxt = document.getElementById("score-number");
+const startGameButton = document.getElementById("start-game");
 
 startGameButton.addEventListener("click", function() {
   instructionModal.style.display = "none";
+  spawnCoins();
   game.gameLoop();
 });
 
@@ -46,14 +48,59 @@ const GAME_WIDTH = 288;
 const GAME_HEIGHT = 512;
 const FLOOR_HEIGHT = 112;
 const FLOOR_WIDTH = 336;
+const FLOOR_START = GAME_HEIGHT - FLOOR_HEIGHT;
 const PLAYER_HEIGHT = 100;
 const PLAYER_WIDTH = 49;
 const IMAGES_PATH = "/game/img/";
-const GRAVITY = 0.2;
-const COIN_WIDTH = 440;
+const GRAVITY = 1.0;
+const COIN_WIDTH = 44;
 const COIN_HEIGHT = 40;
+let game;
 
 // instruction-end
+
+
+// load assets
+let backgroundImage;
+let floorImage;
+let cashBakeManImage;
+let coinImage;
+
+function loadImage(imageUrl, x, y, w, h) {
+  return new Promise((resolve, reject) => {
+    const image = new Image();
+    image.src = `/game/img/${imageUrl}`;
+    image.onload = function() {
+      resolve(image);
+    };
+    image.onabort = function() {
+      reject(`Couldn't load image from /game/img/${imageUrl}`);
+    };
+  });
+}
+
+function loadAllImages() {
+  Promise.all([
+    loadImage("background-day.png", 0, 0, GAME_WIDTH, GAME_HEIGHT),
+    loadImage("floor.png", 0, GAME_HEIGHT - FLOOR_HEIGHT, FLOOR_WIDTH, FLOOR_HEIGHT),    
+    loadImage("cashBakeMan.png"),
+    loadImage("coin-sprite.png")   
+  ]).then(values => {
+    const [
+      background,
+      floor,
+      cashBakeMan,
+      coinSprite    
+    ] = values;
+    backgroundImage = background;
+    floorImage = floor;
+    cashBakeManImage = cashBakeMan; 
+    coinImage = coinSprite;
+    game = new GameArea(1200, 500, "easy");   
+  });
+}
+
+loadAllImages();
 
 // Canvas on global
 const canvas = document.createElement("canvas");
@@ -76,25 +123,78 @@ body.addEventListener("keydown", event => {
   } else direction = 0;
 });
 
-function draw(imageUrl, x, y, w, h, onload = () => {}) {
-  const image = new Image();
-  image.src = `${IMAGES_PATH}${imageUrl}`;
-  image.onload = function() {
-    context.drawImage(image, x, y, w, h);
-    onload();
-  };
-}
 //game (Damian)
+
+let lastTime = 0; //time last loop was executed
+
+let timeToRotateCoinCounter = 0; //is it time to rotate the coin
+const timeToRotateCoin = 100; //coin frame changed every 100 ms
+let timeToSpawnCoin = 2 * 1000; //a coin is spawned every 5s
+let timeToSpawnCoinCounter = 0; //is it time to spawn a new coin
+
 let player = {
   x: (GAME_WIDTH - PLAYER_WIDTH) / 2,
   y: GAME_HEIGHT - FLOOR_HEIGHT - PLAYER_HEIGHT
 };
 
+let coins = [];
+
 function drawPlayer() {
-  draw("cashBakeMan.png", player.x, player.y, PLAYER_WIDTH, PLAYER_HEIGHT);
+  context.drawImage(cashBakeManImage, player.x, player.y, PLAYER_WIDTH, PLAYER_HEIGHT);  
 }
 
-function drawCoin() {}
+function drawCoins() {
+  coins.forEach(coin => {
+    context.drawImage(coinImage, coin.frame * COIN_WIDTH, 0, COIN_WIDTH, COIN_HEIGHT, coin.x, coin.y, COIN_WIDTH, COIN_HEIGHT);
+  });    
+}
+
+function coinFall() {
+  coins.forEach(coin => {
+    coin.y += GRAVITY;
+  });  
+}
+
+function rotateCoins() {
+  coins.forEach(coin => {
+    if (coin.frame == 9) {
+      coin.frame = 0;
+    }
+    else {
+      coin.frame++;
+    }
+  });
+}
+
+function checkCoinPlayerCollision(coin) {
+  let coinx1 = coin.x + COIN_WIDTH;
+  let coiny1 = coin.y + COIN_HEIGHT;
+
+  let playerx1 = player.x + PLAYER_WIDTH;
+  let playery1 = player.y + PLAYER_HEIGHT;
+  
+  if (coin.x < playerx1 && player.x < coinx1 && coin.y < playery1)
+    return player.y < coiny1;
+  else
+    return false;
+}
+
+function spawnCoins() {
+  let coinX = Math.random() * (GAME_WIDTH - COIN_WIDTH);
+  coinX = coinX < 0 ? 0 : coinX;
+  coins.push({
+    x: coinX,
+    y: 0,
+    forRemoval: false,
+    frame: Math.floor(Math.random() * 10)
+  });
+}
+
+function removeCoins() {
+  coins = coins.filter(coin => {
+    return !coin.forRemoval;
+  });
+}
 
 function moveRight() {
   if (player.x < GAME_WIDTH - PLAYER_WIDTH) {
@@ -125,51 +225,64 @@ function GameArea(
 }
 
 GameArea.prototype = {
-  drawMyImage: function(imageUrl, x, y, w, h, onload = () => {}) {
-    this.x = x;
-    this.y = y;
-    this.w = w;
-
-    const image = new Image();
-    image.src = `${IMAGES_PATH}${imageUrl}`;
-    image.onload = function() {
-      context.drawImage(image, x, y, w, h);
-      onload();
-    };
+  drawFloor: function() {
+    context.drawImage(floorImage, 0, GAME_HEIGHT - FLOOR_HEIGHT, FLOOR_WIDTH, FLOOR_HEIGHT);      
   },
-  drawFloor: function(context) {
-    const floorImage = this.drawMyImage(
-      "floor.png",
-      0,
-      GAME_HEIGHT - FLOOR_HEIGHT,
-      FLOOR_WIDTH,
-      FLOOR_HEIGHT
-    );
-  },
-  drawBackground: function(context) {
-    const backgroundImage = this.drawMyImage(
-      "background-day.png",
-      0,
-      0,
-      GAME_WIDTH,
-      GAME_HEIGHT
-    );
+  drawBackground: function() {
+    context.drawImage(backgroundImage, 0, 0, GAME_WIDTH, GAME_HEIGHT);          
   },
   generateCanvas: function() {
-    this.drawBackground(this.context);
-    this.drawFloor(this.context);
-    drawPlayer();
-  },
-  gameLoop: function(time) {
     this.drawBackground();
     this.drawFloor();
     drawPlayer();
+  },
+  gameLoop: function(time) {
+    if (time) {
+      let delta = time - lastTime;
+      lastTime = time;
+      timeToRotateCoinCounter += delta;
+      timeToSpawnCoinCounter += delta;
+    }
+
+    coinFall();
+
+    if (timeToRotateCoinCounter >= timeToRotateCoin) {
+      rotateCoins();            
+      timeToRotateCoinCounter = 0;
+    }
+    
+    coins.forEach(coin => {
+      if (coin.y >= FLOOR_START - COIN_HEIGHT) {
+        console.log("game over");
+        coin.forRemoval = true;
+      }
+    });
+    
+    coins.forEach(coin => {
+      if (checkCoinPlayerCollision(coin)) {
+        console.log("złapano monetę");
+        coin.forRemoval = true;
+      } 
+    });
+    
+    removeCoins();
+
+    if (timeToSpawnCoinCounter >= timeToSpawnCoin) {
+      spawnCoins();
+      timeToSpawnCoinCounter = 0;
+    }
+    
+    this.drawBackground();
+    this.drawFloor();
+    drawPlayer();
+    drawCoins();
 
     requestAnimationFrame(this.gameLoop.bind(this));
   }
 };
 
-const game = new GameArea(1200, 500, "easy");
+//const game = new GameArea(1200, 500, "easy");   
+
 // game.gameLoop();
 
 //TODO: add  loop function
